1b3eeff1d51c67e8e6c129bf418f0ddcd43dd849
Lucia-Galiano <lugaliano03@gmail.com>
2023-06-29 22:29:10 -0300

    Added js date

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-alert("Hola");
+let date = document.querySelector("#date");
+let currentTime = new Date();
+let hours = currentTime.getHours();
+let minutes = currentTime.getMinutes();
+let dayIndex = currentTime.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[dayIndex];
+
+date.innerHTML = `${day} ${hours}:${minutes}`;
