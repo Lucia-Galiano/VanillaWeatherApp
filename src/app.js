1058727f9f188e0cab6430e879e83e5717724a71
Lucia-Galiano <lugaliano03@gmail.com>
2023-06-30 16:05:49 -0300

    Added Font

--- src/app.js
+++ src/app.js
@@ -16,3 +16,33 @@ let days = [
 let day = days[dayIndex];
 
 date.innerHTML = `${day} ${hours}:${minutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city");
+  let input = document.querySelector("#city-input");
+  city.innerHTML = input.value;
+  let apiKey = "bd3bb6534458ba51b48c49f5155745b6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}`).then(showTemp);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+function showTemp(response) {
+  let temp = Math.round(response.data.main.temp);
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = `${temp}`;
+  let h1 = document.querySelector("#city");
+  h1.innerHTML = response.data.name;
+}
+
+let emoji = document.querySelector("#emoji");
+function emojiWeather() {
+  if (temp <= 10) {
+    emoji.innerHTML = "☁️";
+  } else {
+    emoji.innerHTML = "☀️";
+  }
+}
