6322726cdb7417b3d4fd7e124af45e70aaefbe57
Lucia-Galiano <lugaliano03@gmail.com>
2023-07-08 20:01:16 -0300

    Added API to forecast

--- src/app.js
+++ src/app.js
@@ -22,33 +22,39 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Thu", "Fri", "Sat", "Sun"];
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
       <div class="forecast-date">
-        ${day}
+        ${formatDay(forecastDay.dt)}
       </div>
-      <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="" width="42"/>
+      <img src="https://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png" alt="" width="42"/>
     <div class="forecast-temperatures">
-<span class="forecast-temp-max">
-  18°
+<span class="forecast-temp-max"> ${Math.round(forecastDay.temp.max)}°
 </span>
-<span class="forecast-temp-min">
-  12° 
+<span class="forecast-temp-min"> ${Math.round(forecastDay.temp.min)}° 
 </span> 
     </div>
-    </div>
   </div>`;
+    }
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 function getForecast(coordinates) {
   console.log(coordinates);
